7c9b40d56f11c8f72e6a98f9d6a9e21bb1f2ac18
Nikos Monios <moniosni@gmail.com>
2023-02-22 22:15:25 +0200

    fixed path loss exponent for indoors use

--- NS3_simulation/sim.cpp
+++ NS3_simulation/sim.cpp
@@ -153,8 +153,8 @@ int main (int argc, char *argv[])
 
   // Create the lora channel object
   Ptr<LogDistancePropagationLossModel> loss = CreateObject<LogDistancePropagationLossModel> ();
-  loss->SetPathLossExponent (2.3); // was 3.76
-  loss->SetReference (1, 42); // was 1, 7.7
+  loss->SetPathLossExponent (1.7); // was 3.76
+  loss->SetReference (1, 10); // was 1, 7.7
 
   if (realisticChannelModel)
     {
